e7faa0255d69dc251b2208c099517533367c15c0
Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
2020-09-05 18:49:45 +0900

    Seperate inline-js

---
+++ assets/js/table.js
@@ -0,0 +1,8 @@
+function sortTable(TABLE, TBODY) {
+  const tbody = document.getElementById(TBODY);
+  tbody.className = "list";
+  const options = {
+    valueNames: ["市内", "検査確定日", "発症日"],
+  };
+  const userList = new List(TABLE, options);
+}
