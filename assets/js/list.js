4f2efda04cb8fec030baf15c1b3d2fc91c9f425d
Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
2020-11-07 00:31:48 +0900

    Close Issue#143 (#147)
    * Improve script loading
    
    * Modify baseof.html
    
    * Move the location of script files loading
    
    * Improve codes
    
    * Remove preloads
    
    * Update chart.js version

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,8 +1,6 @@
-function sortTable(TABLE, TBODY) {
-  const tbody = document.getElementById(TBODY);
-  tbody.className = "list";
+const sortTable = function (TABLE) {
   const options = {
     valueNames: ["市内", "県内", "検査確定日", "発症日"],
   };
   const userList = new List(TABLE, options);
-}
+};
