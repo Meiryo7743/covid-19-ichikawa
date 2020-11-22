df6e02ad44ee76f19df78a4c44b0d9a48b8fd97d
Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
2020-11-22 20:29:17 +0900

    Fix data format

--- data_updater/patients_count.py
+++ data_updater/patients_count.py
@@ -12,18 +12,20 @@ for i in data:
     with open(dst + i['data_details'], 'r', encoding='utf-8') as f:
         raw = yaml.safe_load(f)
 
-    age_sum = pd.DataFrame(raw)['年代']
+    age = pd.DataFrame(raw)['年代']
+    dict = {}
 
-    age_dict = {}
+    # Count patients by age
+    key = config['age']
+    for j in key:
+        age_bool = (age == j['find'])
+        dict[j['replace']] = int(age_bool.sum())
 
-    # Count by age
-    age = config['age']
-    for j in age:
-        age_bool = (age_sum == j['find'])
-        age_dict[j['replace']] = int(age_bool.sum())
+    # Count total patients
+    dict['total'] = int(age.value_counts().sum())
 
-    # Count total
-    age_dict['total'] = int(age_sum.value_counts().sum())
+    # Convert the data to list type so that Hugo can process them
+    data_count = [dict]
 
     with open(dst + i['data_count'], 'w', encoding='utf-8', newline='\n') as f:
-        yaml.dump(age_dict, f, indent=2, allow_unicode=True)
+        yaml.dump(data_count, f, indent=2, allow_unicode=True)
