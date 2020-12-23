0542e65a349273a5e6e374212dd285e99c74eed4
Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
2020-12-23 23:58:35 +0900

    Update netlify settings

--- data-updater/netlify/get-redirects.py
+++ data-updater/netlify/get-redirects.py
@@ -1,62 +1,21 @@
-import re
 import toml
+import sys
 
-LANGUAGES = ['ja', 'en']
-REDIRECTS = [
-    {
-        'from': '/contacts',
-        'to': 'https://forms.gle/rJAhPqckU1Ak7tFQ9',
-        'force': True
-    },
-    {
-        'from': '/cards/details-of-all-patients',
-        'to': '/cards/details-of-patients-with-symptoms',
-        'force': True
-    },
-    {
-        'from': '/cards/details-of-latest-30-patients',
-        'to': '/cards/details-of-latest-30-patients-with-symptoms',
-        'force': True
-    },
-    {
-        'from': '/cards/the-number-of-patients',
-        'to': '/cards/the-number-of-patients-with-symptoms',
-        'force': True
-    },
-    {
-        'from': '/cards/changes-in-the-number-of-passengers-on-toei-subway',
-        'to': '/cards/changes-in-the-rates-of-toei-subway-passengers',
-        'force': True
-    },
-    {
-        'from': '/*',
-        'to': '/404.html',
-        'force': False,
-        'status': '404'
-    }
-]
+with open('./data-updater/netlify/config.toml', 'r', encoding='utf-8', newline='\n') as f:
+    config: dict = toml.load(f)
 
-data = {
-    'redirects': []
+redirects: dict = {
+    'redirects': [
+        {
+            'from': '/' + j + i['from'],
+            'to': i['to'] if i['to'].startswith('http') else '/' + j + i['to'],
+            'force': i.get('force'),
+            'status': i.get('status'),
+        }
+        for i in config['redirect']
+        for j in config['language']
+    ]
 }
 
-for i in REDIRECTS:
-    for j in LANGUAGES:
-        dict = {}
-
-        dict['from'] = '/' + j + i.get('from')
-
-        if i.get('to').startswith('http'):
-            dict['to'] = i.get('to')
-        else:
-            dict['to'] = '/' + j + i.get('to')
-
-        dict['force'] = i.get('force')
-
-        if not i.get('status') == None:
-            dict['status'] = int(i.get('status'))
-
-        data['redirects'].append(dict)
-
-with open('./static/netlify.toml', 'w', encoding='utf-8', newline='\n') as f:
-    toml.dump(data, f)
+with open(config['dst'], 'w', encoding='utf-8', newline='\n') as f:
+    toml.dump(redirects, f)
